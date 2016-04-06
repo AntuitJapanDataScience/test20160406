4a3b95618a77c4f85ca2283f4be9fdb89121ddfa
kunitaka0605 <kunitaka0605@gmail.com>
2016-04-06 18:30:14 +0900

    Signed-off-by: kunitaka0605 <kunitaka0605@gmail.com>

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+directory="${HOME}/diary"
+
+if [ ! -d "$directory" ]; then
+        mkdir "$directory"
+fi
+
+diaryfile="${directory}/$(date '+%Y-%m-%d-%H:%M:%S').txt"
+
+if [ ! -e "$diaryfile" ]; then
+        date '+%Y/%m/%d-%H:%M:%S' > "$diaryfile"
+fi
+
+#vim "$diaryfile"
