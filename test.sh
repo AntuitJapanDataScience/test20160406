5f48c2fec331e7ad597588479108ea658c1ea3a6
kunitaka0605 <kunitaka0605@gmail.com>
2016-04-06 18:41:15 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,5 +11,7 @@ diaryfile="${directory}/$(date '+%Y-%m-%d-%H:%M:%S').txt"
 if [ ! -e "$diaryfile" ]; then
         date '+%Y/%m/%d-%H:%M:%S' > "$diaryfile"
 fi
+
+#commment 1
 #commment
 #vim "$diaryfile"
